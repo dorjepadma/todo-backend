5385e53afc2ea49556f2eb723c3d9e00939e9657
dorjepadma <dorjepadma@gmail.com>
2020-02-26 09:10:08 -0800

    js files written

---
+++ data/load-seed-data.js
@@ -0,0 +1,28 @@
+const client = require('../lib/client');
+const todos = require('./todos');
+
+run();
+
+async function run () {
+
+try {
+    await client.connect();
+
+    await Promise.all(
+        todos.map(todo => {
+            return client.query(`
+            INSERT INTO todos (taks, complete)
+            VALUES ($1, $2);
+            `,
+            [todo.taks, todo.complete]);
+        })
+    );
+    }
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        client.end();
+    }
+    
+}
