8f802fbc9ba5eeef1590475cdda013dfded2b392
dorjepadma <dorjepadma@gmail.com>
2020-02-27 11:22:43 -0800

    auth routes put in

--- server.js
+++ server.js
@@ -16,6 +16,34 @@ app.use(express.json());
 // ToDos
 app.use(express.urlencoded({ extended: true }));
 
+const createAuthRoutes = require('././lib/auth/create-auth-routes');
+
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash
+        FROM users
+        WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.row[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`INSERT into users (email, hash)
+        VALUES ($1, $2)
+        RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+app.use('/api/auth', authRoutes);
+
+const ensureAuth = require('./lib/auth/ensure-auth');
+
+app.use('/api', ensureAuth);
+
 app.get('/api/todos', async(req, res) => {
 
     try {
